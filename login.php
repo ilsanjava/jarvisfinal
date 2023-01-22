01414854e9496750b11f4c7a7e410dffdb69076a
ilsanjava <106230267+ilsanjava@users.noreply.github.com>
2023-01-22 08:57:32 +0700

    Add files via upload

---
+++ login.php
@@ -0,0 +1,82 @@
+<?php
+	@ob_start();
+	session_start();
+	if(isset($_POST['proses'])){
+		require 'config.php';
+			
+		$user = strip_tags($_POST['user']);
+		$pass = strip_tags($_POST['pass']);
+
+		$sql = 'select member.*, login.user, login.pass
+				from member inner join login on member.id_member = login.id_member
+				where user =? and pass = md5(?)';
+		$row = $config->prepare($sql);
+		$row -> execute(array($user,$pass));
+		$jum = $row -> rowCount();
+		if($jum > 0){
+			$hasil = $row -> fetch();
+			$_SESSION['admin'] = $hasil;
+			echo '<script>alert("Selamat anda berhasil login");window.location="index.php"</script>';
+		}else{
+			echo '<script>alert("Login Gagal");history.go(-1);</script>';
+		}
+	}
+?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="Dashboard">
+    <meta name="keyword">
+
+    <title>Admin</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="assets/css/bootstrap.css" rel="stylesheet">
+    <!--external css-->
+    <link href="assets/font-awesome/css/font-awesome.css" rel="stylesheet" />
+        
+    <!-- Custom styles for this template -->
+    <link href="assets/css/style.css" rel="stylesheet">
+    <link href="assets/css/style-responsive.css" rel="stylesheet">
+
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+
+  <body style="background:#f0f8ff;color:#fff;">
+  <div class="row-0 justify-centent-center text-center">
+  	<img src="assets/img/logoteko.png" width="350px" height="200px" class="justify-content-center align-items-center"/>
+  </div>
+	
+      <!-- **********************************************************************************************************************************************************
+      MAIN CONTENT
+      *********************************************************************************************************************************************************** -->
+
+	  <div id="login-page">
+	  	<div class="container">
+		      <form class="form-login" method="POST">
+		        <h2 class="form-login-heading" ><font color="white" size='5'> <b>TECKO LASER</b></font> <br>
+		        	<font color="white" size='2'>Masukkan Username dan Password</font></h2>
+		        <div class="login-wrap">
+		            <input type="text" class="form-control" name="user" placeholder="Username" autofocus>
+		            <br>
+		            <input type="password" class="form-control" name="pass" placeholder="Password">
+		            <br>
+		            <button class="btn btn-primary btn-block" name="proses" type="submit"><i class="fa fa-lock"></i> SIGN IN</button>
+		        </div>
+		      </form>
+	  	</div>
+	  </div>
+    <!-- js placed at the end of the document so the pages load faster -->
+    <script src="assets/js/jquery.js"></script>
+    <script src="assets/js/bootstrap.min.js"></script>
+    
+  </body>
+</html>
+
